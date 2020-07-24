8142cb5ab767ead7aaa10bf2dfeafb83ae8a1d19
hinajameel <hinajameel229@gmail.com>
2020-07-23 23:27:40 -0700

    submitted

--- chap43-48/app.js
+++ chap43-48/app.js
@@ -1,1 +1,8 @@
 //1. Show an alert box on click on a link.
+function imag(){
+    // document.getElementById('ig').innerHTML = <img src="mob.jpg" alt=""></img>
+
+    var a = document.getElementById("img").innerHTML="hello"
+
+   alert("Thank you ");
+}
